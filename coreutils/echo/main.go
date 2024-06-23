54446c92f489f12f84cde6e9e6d0505db54a4893
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-06-23 20:24:00 +0300

    implemented echo

---
+++ coreutils/echo/main.go
@@ -0,0 +1,17 @@
+package main
+ 
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Print(os.Args[i])
+		if i == len(os.Args) - 1 {
+			fmt.Println()
+		} else {
+			fmt.Print(" ")
+		}
+	}
+}
