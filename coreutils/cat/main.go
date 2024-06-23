6a71276c7c4feab104220350a3655e4b369ba9f7
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-06-23 21:17:57 +0300

    cat update bec i forgot to add flag before

--- coreutils/cat/main.go
+++ coreutils/cat/main.go
@@ -5,6 +5,7 @@ import (
 	"bufio"
 	"os"
 	"io"
+	"flag"
 )
 func check(e error) {
     if e != nil {
@@ -12,25 +13,41 @@ func check(e error) {
     }
 }
 func main() {
+	var lines int
+	flag.IntVar( &lines,"n", -1, "custom number of lines to print")
+	flag.Parse()
+
 	var file_name string
 	var input io.Reader 
 
 	if len(os.Args) == 1 {
 		fmt.Println("No file passed")
 		os.Exit(1)
+	} else if len(os.Args) == 2 {
+		file_name = os.Args[1]
+	} else if len(os.Args) == 4 {
+		file_name = os.Args[3]
 	}
-	file_name = os.Args[1]
+
 	file, err := os.Open(file_name)
 	check(err)
 	
 	input = file
 
 	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		line := scanner.Text()
-		fmt.Println(line)
+	if lines == -1 {
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	} else {
+		for i := 0; i < lines; i++ {
+			if !scanner.Scan() {
+				break
+			}
+			fmt.Println(scanner.Text())
+		}
 	}
-
+	
 	check(scanner.Err())
 	
 }
