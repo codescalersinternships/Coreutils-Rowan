bccb55aa28dc0f460248760b633cb55f7dd04ab5
rowanxgamal <rowangamalelkhouly@gmail.com>
2024-06-24 16:03:19 +0300

    changed implementation of echo to use strings.Join

--- cmd/echo/main.go
+++ cmd/echo/main.go
@@ -3,15 +3,9 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	for i := 1; i < len(os.Args); i++ {
-		fmt.Print(os.Args[i])
-		if i == len(os.Args)-1 {
-			fmt.Println()
-		} else {
-			fmt.Print(" ")
-		}
-	}
+	fmt.Println(strings.Join(os.Args[1:], " "))
 }
